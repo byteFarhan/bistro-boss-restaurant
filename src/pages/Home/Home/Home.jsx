c4c98bfb1578cd5ce374448b61255c6e94d64467
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-22 22:45:58 +0600

    Call us section has been added with responsiveness.

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -23,6 +23,14 @@ const Home = () => {
           </div>
         </Cover>
       </section>
+      {/* Call US section */}
+      <section className="px-5 pb-16 mx-auto max-w-7xl md:pb-20 lg:pb-28">
+        <div className="py-10 text-3xl font-semibold text-center text-white md:py-16 lg:py-24 md:text-4xl lg:text-5xl bg-title font-cinzel">
+          <h4 className="flex flex-col justify-center gap-2 md:flex-row md:items-center">
+            <span className="font-inter">Call Us:</span> +88 0192345678910
+          </h4>
+        </div>
+      </section>
     </section>
   );
 };
