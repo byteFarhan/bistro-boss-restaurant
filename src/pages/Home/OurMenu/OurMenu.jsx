67a82483774f992d44220494536555764d739625
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-26 00:48:13 +0600

    load popular menus data form database and display them.

--- src/pages/Home/OurMenu/OurMenu.jsx
+++ src/pages/Home/OurMenu/OurMenu.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import Section from "../../Shared/Section/Section";
 import SectionIntro from "../../Shared/SectionIntro/SectionIntro";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const OurMenu = () => {
+  const [popularMenus, setPopularMenus] = useState([]);
+  const axiosSecure = useAxiosSecure();
+  useEffect(() => {
+    axiosSecure.get("/popular-menus").then((res) => {
+      setPopularMenus(res.data);
+    });
+  }, [axiosSecure]);
+  console.log(popularMenus);
   return (
     <>
       <Section>
         <SectionIntro subTitle={`Check it out`} title={"FROM OUR MENU"} />
-        <div className="grid grid-cols-1 gap-6 mt-6 md:mt-8 lg:mt-12 lg:grid-cols-2 lg:gap-8"></div>
+        <div className="grid grid-cols-1 gap-6 mt-6 md:mt-8 lg:mt-12 lg:grid-cols-2 lg:gap-8">
+          {popularMenus?.length > 0 &&
+            popularMenus.map((menu) => <MenuItem key={menu._id} menu={menu} />)}
+        </div>
+        <div className="mt-6 text-center md:8 lg:10">
+          <button className="px-6 py-4 mt-6 text-lg font-medium uppercase transition-all delay-200 bg-white border-b-2 rounded-lg text-title hover:border-transparent hover:bg-title hover:text-white border-title">
+            View Full Menu
+          </button>
+        </div>
       </Section>
     </>
   );
--- src/pages/Shared/MenuItem/MenuItem.jsx
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -1,10 +1,10 @@
-import img from "../../../assets/home/slide2.jpg";
-const MenuItem = () => {
+const MenuItem = ({ menu }) => {
+  const { name, price, image, recipe } = menu;
   return (
     <div className="flex flex-col items-center gap-6 md:flex-row">
       <div>
         <img
-          src={img}
+          src={image}
           alt=""
           className="bg-natural w-[118px] h-[104px] object-cover cursor-pointer rounded-[200px] rounded-tl-none"
         />
@@ -12,15 +12,11 @@ const MenuItem = () => {
       <div className="text-center md:text-left">
         <span className="flex flex-col items-center justify-center md:justify-between md:flex-row">
           <h4 className="flex items-center gap-2 text-xl font-medium font-cinzel text-title">
-            {`ROAST DUCK BREAST`}{" "}
-            <span className="hidden md:block">------------------</span>
+            {name} <span className="hidden md:block">------------------</span>
           </h4>
-          <p className="text-xl text-dark-orange">${`14.5`}</p>
+          <p className="text-xl text-dark-orange">${price}</p>
         </span>
-        <p className="mt-2 text-natural">
-          Roasted duck breast (served pink) with gratin potato and a griottine
-          cherry sauce
-        </p>
+        <p className="mt-2 text-natural">{recipe}</p>
       </div>
     </div>
   );
