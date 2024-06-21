a35b28074fc161e89ee8a3966892442695acdb4f
farhan-mazumder <farhanmazumder75@gmail.com>
2024-06-21 10:30:21 +0600

    Add functionalities for adding food items to the cart and displaying them using TanStack Query.

--- src/pages/Home/ChefRecommends/FoodCard/FoodCard.jsx
+++ src/pages/Home/ChefRecommends/FoodCard/FoodCard.jsx
@@ -1,7 +1,63 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import cardImg from "../../../../assets/home/slide1.jpg";
+import useAuth from "../../../../hooks/useAuth";
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import useCart from "../../../../hooks/useCart";
 
 const FoodCard = ({ item, showPrice }) => {
-  const { name, recipe, image, price } = item;
+  const axiosSecure = useAxiosSecure();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  const [, refetch] = useCart();
+  const { name, recipe, image, price, _id } = item;
+  const handleAddToCart = (item) => {
+    // console.log(item);
+    // console.log(user?.email);
+    if (user && user?.email) {
+      const cartItem = {
+        menuId: _id,
+        price,
+        image,
+        name,
+        email: user?.email,
+      };
+      axiosSecure
+        .post("/cart", cartItem)
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.insertedId) {
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: `${name} added to the cart.`,
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            refetch();
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      Swal.fire({
+        title: "You are not logged in!",
+        text: "You have to login your account for adding item to cart!",
+        icon: "info",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes Login",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location }, replace: true });
+        }
+      });
+    }
+  };
   return (
     <>
       <div className="w-full overflow-hidden bg-[#F3F3F3] rounded-sm">
@@ -27,7 +83,12 @@ const FoodCard = ({ item, showPrice }) => {
               <p className="text-title">{recipe}</p>
             </div>
           </div>
-          <button className="btn-base hover:bg-[#1F2937] border-[#BB8506] bg-[#E8E8E8] text-dark-orange">
+          <button
+            onClick={() => {
+              handleAddToCart(item);
+            }}
+            className="btn-base hover:bg-[#1F2937] border-[#BB8506] bg-[#E8E8E8] text-dark-orange"
+          >
             add to cart
           </button>
         </div>
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -4,10 +4,13 @@ import { Link } from "react-router-dom";
 import NavItem from "./NavItem/NavItem";
 import useAuth from "../../../hooks/useAuth";
 import toast from "react-hot-toast";
+import { IoCartOutline } from "react-icons/io5";
+import useCart from "../../../hooks/useCart";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { user, userSignOut } = useAuth();
+  const [cart] = useCart();
   const handleLogout = () => {
     userSignOut()
       .then(() => {
@@ -78,20 +81,12 @@ const Navbar = () => {
                       className="hidden mx-4 text-gray-600 transition-colors duration-300 transform lg:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none"
                       aria-label="show notifications"
                     >
-                      <svg
-                        className="w-6 h-6"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
+                      <div className="relative">
+                        <IoCartOutline className="text-2xl font-semibold lg:text-3xl" />
+                        <p className="absolute top-[-10px] right-[-12px] text-lg bg-white rounded-full font-medium size-6 text-title">
+                          {cart?.length}
+                        </p>
+                      </div>
                     </button>
 
                     <button
